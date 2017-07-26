f6a8c70d7e35bba3a2ed29678234df1b78bd9f0b
Canglangweiwei <weiweistar@163.com>
2017-07-26 14:58:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
@@ -0,0 +1,11 @@
+package com.example.gsyvideoplayer;
+
+import android.app.Application;
+
+public class GSYApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
